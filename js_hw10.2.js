78d421f80de483fb8e0e4b35e84340e163ee8096
KravaV <KravaVlad10.01@gmail.com>
2025-09-24 21:44:04 +0200

    Remove unnecessary files

--- js_hw10.2.js
+++
@@ -1,6 +0,0 @@
-const text = "Wonderful Happiness, Time, Task, Apple Joyful titles toy";
-//const text = prompt("Введіть рядок:");
-const pattern =/\b[^Aa\s]{6,}\b/g;
-
-const result = text.match(pattern);
-console.log(result);
